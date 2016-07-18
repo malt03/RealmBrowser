1887c6beb2a48b43a7dc94bc73d9b0b7d1f457f9
Koji Murata <koji.murata@dena.com>
2016-07-19 02:01:11 +0900

    fix example

--- Example/RealmBrowser/Child.swift
+++ Example/RealmBrowser/Child.swift
@@ -10,15 +10,4 @@ import UIKit
 import RealmSwift
 
 class Child: Object {
-  dynamic var boolValue = true
-  dynamic var intValue = 0
-  dynamic var doubleValue = Double(0)
-  dynamic var floatValue = Float(0)
-  dynamic var stringValue = ""
-  dynamic var dateValue = NSDate()
-  dynamic var dataValue = NSData()
-  
-  override class func primaryKey() -> String? {
-    return "stringValue"
-  }
 }
--- Example/RealmBrowser/Example.swift
+++ Example/RealmBrowser/Example.swift
@@ -10,5 +10,28 @@ import UIKit
 import RealmSwift
 
 class Example: Object {
-
+  dynamic var boolValue = true
+  dynamic var int8Value = Int8(0)
+  dynamic var int16Value = Int16(0)
+  dynamic var int32Value = Int32(0)
+  dynamic var int64Value = Int64(0)
+  dynamic var doubleValue = Double(0)
+  dynamic var floatValue = Float(0)
+  dynamic var stringValue = ""
+  dynamic var dateValue = NSDate()
+  dynamic var dataValue = NSData()
+  let optionalBoolValue = RealmOptional<Bool>(true)
+  let optionalIntValue = RealmOptional<Int>(0)
+  let optionalDoubleValue = RealmOptional<Double>(0)
+  let optionalFloatValue = RealmOptional<Float>(0)
+  dynamic var optionalStringValue: String? = ""
+  dynamic var optionalDateValue: NSDate? = NSDate()
+  dynamic var optionalDataValue: NSData? = NSData()
+  
+  var child: Child?
+  let children = List<Child>()
+  
+  override class func primaryKey() -> String? {
+    return "stringValue"
+  }
 }
